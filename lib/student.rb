a8f78f42055bfd8930c5f2732f63afdc88e24223
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 11:03:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ class Student
     end.first
   end
 
-  def self.first_X_students_in_grade_10(number)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT *
       FROM students
