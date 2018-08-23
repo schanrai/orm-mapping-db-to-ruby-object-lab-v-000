6d9881f7a3fbb476804a29934ac8fa4f7a6505f4
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 09:31:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,14 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+    SQL
+
+    DB[:conn].exceute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
