7f1192caf6425fe34a5448322dbd13ee2ecc740c
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 10:36:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
       FROM students
       WHERE grade = 9
     SQL
-    DB[:conn].execute(sql, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
