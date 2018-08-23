56511b02a66934ced1a118a25267cee27bad9c44
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 10:00:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,8 @@ class Student
     SQL
     DB[:conn].execute(sql, name).map do |row|
       #binding.pry
-      self.new_from_db(row)[1]
+      instance = self.new_from_db(row)
+      instance[1]
     end
   end
 
