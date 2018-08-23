13ea2278bee0f118c89d0adaf47e3a7dfb52dedb
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 10:23:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,9 +26,9 @@ class Student
   end
 
 
-  def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
       FROM students
