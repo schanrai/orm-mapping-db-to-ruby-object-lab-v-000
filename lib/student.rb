bb955c0fcd965e565308cf67a79bf89902da1d81
Sushi Chanrai <sushi@pathworks.co>
2018-08-23 10:37:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 9
+      WHERE grade < 12
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
